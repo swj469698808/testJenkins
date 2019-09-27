a1d0af3fc9dc8dd255b00aaf407ad9a48dd79a5b
sunweijie <sunweijie@piesat.cn>
2019-09-27 13:26:06 +0800

    测试接口的编写

---
+++ src/main/java/testjenkins/jenkins/Controller/TestController.java
@@ -0,0 +1,21 @@
+package testjenkins.jenkins.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: sunwj
+ * @CreateDate: 2019/9/27 11:10
+ */
+@RestController
+public class TestController {
+
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String Test(){
+        return "我是第一版的jenkins";
+    }
+}
+
---
+++ src/main/java/testjenkins/jenkins/JenkinsApplication.java
@@ -0,0 +1,15 @@
+package testjenkins.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+}
